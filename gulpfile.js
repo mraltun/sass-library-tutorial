3e8333a482bc9623d361e2bb03d193fa850be8e0
Murat Altun <mraltun@gmail.com>
2022-12-16 17:38:30 +0300

    Moved base css to _base file

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ const sass = require("gulp-sass")(require("sass"));
 
 // Compile the CSS from SCSS
 function buildStyles() {
-  return src("*.scss").pipe(sass()).pipe(dest("css"));
+  return src("customlib/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 // Watch the changes and re-compile.
 function watchTask() {
-  watch(["*.scss"], buildStyles);
+  watch(["customlib/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
