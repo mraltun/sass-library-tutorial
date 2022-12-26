92edcb92eee6475bbbded74b79903be51647c097
Murat Altun <mraltun@gmail.com>
2022-12-26 13:24:34 +0300

    Added gulp-purgecss to purge unused styles from compiled css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 // Compile the CSS from SCSS
 function buildStyles() {
-  return src("customlib/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("customlib/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 // Watch the changes and re-compile.
